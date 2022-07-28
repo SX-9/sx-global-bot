b7cdab44266d23b5104374fbafa509f8e0541322
SX Spy Agent <90962949+SX-9@users.noreply.github.com>
2022-07-28 16:48:24 +0700

    join and leave message

--- index.js
+++ index.js
@@ -12,22 +12,29 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.author.bot) return;
+    let cache = client.channels.cache;
+    let channel = msg.channel;
+    let embed2 = new Discord.MessageEmbed()
+        .setAuthor("System", msg.guild.iconURL())
+        .setTimestamp(new Date())
+        .setFooter("Sent From " + msg.guild.name + " | Made By sx9.is-a.dev");
     if (msg.content.startsWith("mv!register")) {
         if (channels.includes(msg.channel.id)) return msg.channel.send("This channel is already registered!");
         if (!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("You need to be an ADMINISTRATOR to register this channel!");
         channels.push(msg.channel.id);
         fs.writeFileSync('./db.json', JSON.stringify(channels));
         msg.channel.send("Current channel registered.");
+        channels.forEach(channel => {
+            cache.get(channel).send(embed2.setColor('GREEN').setDescription(`${msg.guild.name} has joined us!`));
+        });
         return;
     }
-    let embed = new Discord.MessageEmbed()
+    let embed1 = new Discord.MessageEmbed()
         .setColor('BLUE')
         .setAuthor(msg.author.username, msg.author.avatarURL())
         .setDescription(`${msg.content}`)
         .setTimestamp(new Date())
         .setFooter("Sent From " + msg.guild.name + " | Made By sx9.is-a.dev");
-    let cache = client.channels.cache;
-    let channel = msg.channel;
     channels.forEach(c1 => {
         if (channel.id === c1) {
             msg.delete();
@@ -35,13 +42,16 @@ client.on('message', msg => {
                 try {
                     if (msg.attachments.size > 0) {
                         msg.attachments.forEach(a => {
-                            cache.get(c2).send(embed.attachFiles(a.url));
+                            cache.get(c2).send(embed1.attachFiles(a.url));
                         });
                     } else {
-                        cache.get(c2).send(embed);
+                        cache.get(c2).send(embed1);
                     }
                 } catch {
                     delete channels[c2];
+                    channels.forEach(c3 => {
+                        cache.get(c3).send(embed2.setColor('RED').setDescription(`${msg.guild.name} has left us!`));
+                    });
                 }
             });
         }
