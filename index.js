24ca016a05d3078b3ae41914b3f28907e4739f4b
sx9dev <hello@mail.sx9.is-a.dev>
2022-11-26 15:04:11 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ 
+const client = new Discord.Client({
     intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES']
 });
+const app = require('express')();
+const swears = require('badwords/array');
 const fs = require('fs');
 const channels = require('./db.json');
 
+let links = [ 'https://', 'http://', 'www.', '.com' ]
+let bannedWords = swears.concat(links);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(3000, () => console.log('Starting...'))
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('mv!register', { type: 'LISTENING' });
+    client.user.setActivity(`mv!register | ${channels.length} Universes`, { type: 'LISTENING' });
 });
 
 client.on('message', msg => {
@@ -21,18 +32,27 @@ client.on('message', msg => {
         .setDescription(`${msg.guild.name} has joined us!`)
         .setFooter("Sent From " + msg.guild.name + " | Made By sx9.is-a.dev");
     if (msg.content.startsWith("mv!register")) {
-        if (channels.includes(msg.channel.id)) return msg.channel.send("This channel is already registered!");
-        if (!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("You need to be an ADMINISTRATOR to register this channel!");
-        channels.push(msg.channel.id);
+        if (channels.includes(channel.id)) return channel.send("This channel is already registered!");
+        if (!msg.member.hasPermission("ADMINISTRATOR")) return channel.send("You need to be an ADMINISTRATOR to register this channel!");
+        channels.push(channel.id);
         fs.writeFileSync('./db.json', JSON.stringify(channels));
-        msg.channel.send("Current channel registered.");
+        channel.send("Current channel registered.");
         channels.forEach(channel => {
             try {
                 cache.get(channel).send(embed2);
-            } catch {} 
+            } catch {}
         });
         return;
     }
+    let stopLoop = false;
+    bannedWords.forEach((word) => {
+        if (stopLoop) return;
+        if (msg.content.toLowerCase().includes(word)) {
+            try { msg.delete(); } catch { return; };
+            channel.send("Banned Word Detected.");
+            stopLoop = true;
+        }
+    });
     let embed1 = new Discord.MessageEmbed()
         .setColor('BLUE')
         .setAuthor(msg.author.username, msg.author.avatarURL())
@@ -40,8 +60,9 @@ client.on('message', msg => {
         .setTimestamp(new Date())
         .setFooter("Sent From " + msg.guild.name + " | Made By sx9.is-a.dev");
     channels.forEach(c1 => {
+        if (stopLoop) return;
         if (channel.id === c1) {
-            msg.delete();
+            try { msg.delete(); } catch { return; };
             channels.forEach(c2 => {
                 try {
                     if (msg.attachments.size > 0) {
